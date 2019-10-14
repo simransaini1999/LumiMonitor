b5f54853d698b03415929ab4b1749b248e5a7dbb
KyleV1999 <54862039+KyleV1999@users.noreply.github.com>
2019-10-14 00:19:37 -0400

    Started Main UI. Added play, pause and stop button for music playback. Play button somewhat functional. Added temp logo and colours.

--- app/src/main/java/com/example/lumimonitor/MainActivity.java
+++ app/src/main/java/com/example/lumimonitor/MainActivity.java
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Menu menu;
+    ImageView playbutton;
+    ImageView stopbutton;
+    TextView songplaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playbutton = findViewById(R.id.playbutton);
+        stopbutton = findViewById(R.id.stopbutton);
+                songplaying = findViewById(R.id.SongPlaying);
+
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == findViewById(R.id.playbutton)){
+                    playbutton.setImageResource(R.drawable.ic_pause_black_24dp);
+                    songplaying.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
     public boolean onPrepareOptionsMenu(Menu menu){
         this.menu = menu;
