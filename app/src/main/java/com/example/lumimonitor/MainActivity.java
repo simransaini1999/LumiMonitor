ba5102a10fd9a5df0bcf1dba47cdfc73aed9228a
KyleV1999 <54862039+KyleV1999@users.noreply.github.com>
2019-10-14 01:52:13 -0400

    Implemented a button to go to a new activity.

--- app/src/main/java/com/example/lumimonitor/MainActivity.java
+++ app/src/main/java/com/example/lumimonitor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lumimonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,15 +17,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView stopbutton;
     TextView songplaying;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configureLumiButton();
+
         playbutton = findViewById(R.id.playbutton);
         stopbutton = findViewById(R.id.stopbutton);
-                songplaying = findViewById(R.id.SongPlaying);
+        songplaying = findViewById(R.id.SongPlaying);
 
         playbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +41,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void configureLumiButton(){
+        ImageView ToLumiMonitor;
+        ToLumiMonitor = findViewById(R.id.ToLumiMonitor);
+
+        ToLumiMonitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Lumi_Monitor.class));
+            }
+        });
+
+    }
+
+    /*
     public boolean onPrepareOptionsMenu(Menu menu){
         this.menu = menu;
         return super.onPrepareOptionsMenu(menu);
-    }
+    } */
 }
