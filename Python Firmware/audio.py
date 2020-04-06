f87ec8d9602a9474f9347b3033a7ac732114cd18
KyleV1999 <54862039+KyleV1999@users.noreply.github.com>
2020-04-06 01:51:47 -0400

    Add files via upload

--- Python Firmware/audio.py
+++ Python Firmware/audio.py
@@ -1,6 +1,8 @@
 import pyrebase
 from getpass import getpass
 import vlc
+import math
+import time
 
 config = {
     "apiKey": "AIzaSyAATblfFfI17pRFTFmk7V0SXDTQV10FUDI",
@@ -30,24 +32,41 @@ db = firebase.database()
 def get_song():
     songName = db.child("songPlaying/"+login['localId']+"/songName").get()
     return songName.val()
+
+def get_state():
+    songState = db.child("songPlaying/"+login['localId']+"/songState").get()
+    return songState.val()
     
     
 def get_song_file(songName):
     if(songName == "Alphabet Song"):
-       songFile = "Alphabet_Song.mp3" 
+       songFile = "The ABC Song.mp3" 
        return (songFile)
     elif (songName == "Old MacDonald"):
-       songFile = "Old_MacDonald.mp3" 
+       songFile = "Old MacDonald.mp3" 
+       return (songFile)
+    elif (songName == "Wheels on the Bus"):
+       songFile = "Wheels On The Bus.mp3" 
+       return (songFile)
+    elif (songName == "Twinkle Twinkle Little Star"):
+       songFile = "Twinkle Twinkle Little Star.mp3" 
+       return (songFile)
+    elif (songName == "Down by the Bay"):
+       songFile = "Down By The Bay.mp3" 
        return (songFile)
     else:
      songFile = "none"
      return songFile
 
 songName = get_song()
+songState = get_state()
 fileName = get_song_file(songName)
     
-if (fileName != "none"):
+if (fileName != "none" and songState == "Play"):
     player = vlc.MediaPlayer(fileName)
+    song_len = player.get_length()
+    print ("Playing ", fileName)
+    print ("Song length" , song_len)
     player.play()
 else:
     player = vlc.MediaPlayer()
@@ -55,14 +74,38 @@ else:
     
 while True:
     check_song = get_song()
-    if (songName != check_song and check_song != "No Song Selected!"):
+    check_state = get_state()
+   
+    if (songName != check_song and check_song != "No Song Selected!"): #change song
          player.stop()
          songName = check_song
          fileName = get_song_file(songName)
          player = vlc.MediaPlayer(fileName)
          player.play()
-    elif (check_song == "No Song Selected!"):
+    elif (check_state == "Stop"):
          player.stop()
+    elif (check_state == "Pause"):
+         player.pause()         
+    elif (songName == check_song and check_state == "Play"):
+         player.play()
+                  
+    song_pos = round(player.get_position(),2)
+    print (song_pos)
+    if (song_pos == 1.0):
+        print ("Song ", songName," has finished playing")
+        player.stop()
+        a = {"songName": "No Song Selected", "songState": "Stop"}    
+        try:
+            db.child("songPlaying/"+login['localId']).update(a)
+            print("Database writing success!")
+        except:
+            print ("Database cannot be written, check the permission on database")
+        time.sleep (1.0)
+            
+            
+        
+    
+        
             
         
         
--- Python Firmware/neoringdb.py
+++ Python Firmware/neoringdb.py
@@ -4,67 +4,111 @@ import time
 import math
 import board
 import neopixel
+import busio
+import adafruit_tsl2561
 from getpass import getpass
 
 #pip3 install pyrebase
 
-config = {
+def databaseinit():
+    global db
+    global login
+    config = {
         "apiKey": "AIzaSyAATblfFfI17pRFTFmk7V0SXDTQV10FUDI",
-    "authDomain": "lumimonitor-6745f.firebaseapp.com",
-    "databaseURL": "https://lumimonitor-6745f.firebaseio.com",
-    "projectId": "lumimonitor-6745f",
-    "storageBucket": "lumimonitor-6745f.appspot.com",
-    "messagingSenderId": "89734507168",
-    "appId": "1:89734507168:web:6644f871187a08c3b777c5",
-    "measurementId": "G-KC3JKW9Y9L"
-}
-  
-  
-  
-  
-firebase = pyrebase.initialize_app(config)
-  
-auth = firebase.auth()
-  
-print ("hello")
-  
-email = input("Enter Your Email Address: ")
-password = getpass("Enter Password: ")
-  
-login = auth.sign_in_with_email_and_password(email, password)
-  
-print ("success!")
-db = firebase.database()
+        "authDomain": "lumimonitor-6745f.firebaseapp.com",
+        "databaseURL": "https://lumimonitor-6745f.firebaseio.com",
+        "projectId": "lumimonitor-6745f",
+        "storageBucket": "lumimonitor-6745f.appspot.com",
+        "messagingSenderId": "89734507168",
+        "appId": "1:89734507168:web:6644f871187a08c3b777c5",
+        "measurementId": "G-KC3JKW9Y9L",
+        "serviceAccount": "/home/pi/Desktop/LumiMonitor/serviceAccountCredentials.json"
+      }
+    
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    
+    email = input("Enter Your Email Address: ")
+    password = getpass("Enter Password: ")
+    login = auth.sign_in_with_email_and_password(email, password)
+    
+    print ("success!")
+    db = firebase.database()
+    
 
 def get_values():
     red = db.child("lumiColour/"+login['localId']+"/red").get()
     green = db.child("lumiColour/"+login['localId']+"/green").get()
     blue = db.child("lumiColour/"+login['localId']+"/blue").get()
+    affect = db.child("lumiColour/"+login['localId']+"/affectState").get()
     print("red: ",red.val())
     print("blue: ",blue.val())
     print("green: ",green.val())
-    return(green.val(),red.val(),blue.val())
+    print("green: ",green.val())
+    print("affect: ",affect.val())
+    return(green.val(),red.val(),blue.val(),affect.val())
 
 
 
 
+def main():
+    databaseinit()
+    
+    i2c = busio.I2C(board.SCL, board.SDA)
+    tsl = adafruit_tsl2561.TSL2561(i2c)
+    
+    pixel_pin = board.D18
+    num_pixels = 16
+    ORDER = neopixel.GRB
+    
+    pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False,
+                             pixel_order=ORDER)
+    
+    
+    print("Configuring TSL2561...")
+    tsl.enabled = True
+    time.sleep(1)
 
-pixel_pin = board.D18
-num_pixels = 16
-ORDER = neopixel.GRB
+    # Set gain 0=1x, 1=16x
+    tsl.gain = 0
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False,
-                           pixel_order=ORDER)
+    # Set integration time (0=13.7ms, 1=101ms, 2=402ms, or 3=manual)
+    tsl.integration_time = 1
 
+    print("Getting readings...")
 
-while True:
-    colours = get_values()
-    green = colours[0]
-    red = colours[1]
-    blue = colours[2]
-    pixels.fill((red, green, blue))
-    pixels.show()
-    time.sleep(0.25)
+    # Get raw (luminosity) readings individually
+    broadband = tsl.broadband
+    infrared = tsl.infrared
+    
+    
+    
+    
+    while True:
+        lux = tsl.lux
+        ref = get_values()
+        affect = ref[3]
+        
+        if (lux > 750 and not None) and (affect != 1):
+            print("Lux = {}".format(lux))
+            pixels.fill((0, 0, 0))
+            pixels.show()
+            time.sleep(0.25)
+            
+        else:        
+            colours = get_values()
+            green = colours[0]
+            red = colours[1]
+            blue = colours[2]
+            
+              
+            pixels.fill((red, green, blue))
+            pixels.show()
+            time.sleep(0.25)
+    
+    
+if __name__ == '__main__':
+    main()
 
     
     
